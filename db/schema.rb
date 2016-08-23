b45b148eb5e0be85a0551073c9307c5f345d6212
lily-xue <414564926@qq.com>
2016-08-23 08:36:55 -0700

    add user name model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821071035) do
+ActiveRecord::Schema.define(version: 20160823153043) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160821071035) do
     t.boolean  "is_finished"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
